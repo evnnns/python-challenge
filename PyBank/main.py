ebb9964fbf5f9115e7c82176bb1f9ed55150f3e4
Dan Evans <65984016+evnnns@users.noreply.github.com>
2020-07-10 14:48:54 -0500

    CSV reader working in PyPoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,3 +3,4 @@ import csv
 
 # Path to collect data from the Resources folder
 budget_csv = os.path.join('Resources', 'budget_data.csv')
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,3 +3,15 @@ import csv
 
 # Path to collect data from the Resources folder
 election_csv = os.path.join('Resources', 'election_data.csv')
+
+total_votes = 0
+
+#Copying in CSV reader from previous exercises
+with open(election_csv) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    election_data = csv.reader(csvfile, delimiter=',')
+
+    #Checking CSV Reader Works
+    election_header = next(election_data)
+    print(f'CSV Header: {election_header}')
